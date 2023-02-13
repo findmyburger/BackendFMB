2cbc48b916a6f4736c0f567e6456997233cdb8b9
martins <martincabezosvalle@gmai.com>
2023-02-13 20:19:14 +0100

    Arregilillos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('image')->nullable();
+            //$table->string('image')->nullable($value = null);
             $table->timestamps();
         });
     }
